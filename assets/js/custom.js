88bf09d5e816490a09cbfb456444e3b02a30865e
Brandowo <gogistyle1919@gmail.com>
2020-07-30 19:32:54 +0200

    main slide height fix

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -91,3 +91,7 @@ $(document).ready(function(){
       $('body,html').animate({scrollTop: top}, 1500);
   });
 });
+$(document).ready(function(){
+	/* делаем плавное появление */
+        $(".card").fadeIn('slow');
+});
