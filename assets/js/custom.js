a6dbc561eb5c9c92a0ca886533fb112f59ce8443
Brandowo <gogistyle1919@gmail.com>
2020-08-03 19:25:31 +0200

    Text and buttons added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -92,6 +92,14 @@ $(document).ready(function(){
   });
 });
 $(document).ready(function(){
+  $(".butt-s").on("click","a", function (event) {
+      event.preventDefault();
+      var id  = $(this).attr('href'),
+          top = $(id).offset().top;
+      $('body,html').animate({scrollTop: top}, 1500);
+  });
+});
+$(document).ready(function(){
 	/* делаем плавное появление */
         $(".card").fadeIn('slow');
 });
