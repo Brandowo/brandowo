f8909eec2d44e656461a581d6c36357641831cfe
Brandowo <gogistyle1919@gmail.com>
2020-08-07 20:16:04 +0200

    php form sending

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -161,3 +161,18 @@ $('#one-inp').focus(function(){
     $('.three-inp').focusout(function(){
     $('.thre-lab').removeClass('thre-lab-act ')
     })
+    function call() {
+      var msg   = $('#formx').serialize();
+         $.ajax({
+           type: 'POST',
+           url: 'formx.php', //обращаемся к обработчику
+           data: msg,
+          success: function(data) {  //в случае успеха выводим результаты в div "results"
+              $('#formx').remove(); //скрываем форму после отправки 
+              $('#results').html(data); //показываем сообщение об успехе вместо неё 
+           },
+           error:  function(xhr, str){ //ошибка выводит соответствующее сообщение 
+        alert('Возникла ошибка: ' + xhr.responseCode);
+           }
+         });
+     }
